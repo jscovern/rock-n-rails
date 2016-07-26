9807847f236b9e4b830a048f82a2cf2c76092b1b
James Scovern <jamie.scovern@gmail.com>
2016-07-26 15:31:12 -0600

    finished lab

--- records/app/controllers/records_controller.rb
+++ records/app/controllers/records_controller.rb
@@ -13,4 +13,19 @@ class RecordsController < ApplicationController
 		@record = Record.new
 		render :new
 	end
+
+	def create
+		Record.create(record_params)
+		redirect_to('/records')
+	end
+
+
+
+
+	private
+
+    def record_params
+      params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+    end
+
 end
